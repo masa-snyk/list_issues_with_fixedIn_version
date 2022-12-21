7645d212da1e525342299b66ffbacea37d1a1b23
masatomo-ito2 <45160975+masatomo-ito2@users.noreply.github.com>
2022-12-21 19:38:59 +0900

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # list_issues_with_fixedIn_version
+
+Python script to created CSV file with issues together with FixedIn version.
+
+## Prerequite
+
+- Python
+- `pip install pysnyk`
+  - https://pypi.org/project/pysnyk/
+
+## Usage
+
+```
+python3 list_issues_with_fixedIn_version.py --orgId <Org Id>--projectId <Project ID>
+```
---
+++ list_issues_with_fixedIn_version.py
@@ -0,0 +1,45 @@
+import argparse
+
+from snyk import SnykClient
+from utils import get_default_token_path, get_token
+
+def parse_command_line_args():
+    parser = argparse.ArgumentParser(description="Snyk API Examples")
+    parser.add_argument(
+        "--orgId", type=str, help="The Snyk Organisation Id", required=True
+    )
+    parser.add_argument(
+        "--projectId", type=str, help="The project ID in Snyk", required=True
+    )
+    return parser.parse_args()
+
+def listToString(s):
+    str1 = ""
+    if s:
+        for ele in s:
+            str1 += ele + " "
+    return str1
+
+snyk_token_path = get_default_token_path()
+snyk_token = get_token(snyk_token_path)
+args = parse_command_line_args()
+org_id = args.orgId
+project_id = args.projectId
+
+client = SnykClient(snyk_token)
+issue_set = client.organizations.get(org_id).projects.get(project_id).issueset_aggregated.all()
+
+
+lst_output = []
+
+print("pkgName, pkgVersions, Title, FixedIn")
+
+for v in issue_set.issues:
+    print('%s, %s, %s, %s' %
+      (
+        v.pkgName,
+        listToString(v.pkgVersions),
+        v.issueData.title, 
+        listToString(v.fixInfo.fixedIn)
+      )
+    )
